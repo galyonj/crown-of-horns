96a138ca33b68a2c2468e5bd84acb7e512dac59b
John Galyon <galyon.jb@gmail.com>
2017-11-16 21:30:18 -0500

    pretty much done with the main site, beginning work on porting Donno's site over now.

--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,7 @@ gulp.task( 'lint', function() {
 
 gulp.task( 'scripts', function() {
 	return gulp
-		.src( [ 'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js', 'node_modules/smooth-scroll/dist/js/smooth-scroll.js', 'node_modules/moment/moment.js', 'node_modules/bootstrap-datetime-picker/js/bootstrap-datetimepicker.js', 'dev/' + dir + 'js/assets/*', 'dev/' + dir + '/js/main.js' ] )
+		.src( [ 'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js', 'node_modules/smooth-scroll/dist/js/smooth-scroll.js', 'dev/' + dir + '/js/main.js' ] )
 		.pipe( concat( 'main.js' ) )
 		.pipe( rename( { suffix: '.min' } ) )
 		.pipe( uglify() )
